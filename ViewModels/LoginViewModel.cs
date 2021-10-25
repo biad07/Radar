723675019e913e316a65c17aaf81c4b331b938e6
biad07 <75955961+biad07@users.noreply.github.com>
2021-10-25 14:47:00 +0200

    try

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Caliburn.Micro;
+
+namespace Radar.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string Username1 { get; set; }
+        public string Password1 { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
